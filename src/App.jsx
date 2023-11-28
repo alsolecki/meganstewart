bcab8ddcb181853c3cf7f24aaef1ca6979e4b0da
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-11-28 14:41:42 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Megan Stewart</h1>
+        <p>Executive Coaching Professional</p>
+      </div>
+
+      <div>
+        <h2>About</h2>
+        <p>Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia. Nulla ex sunt eiusmod quis. Tempor laborum qui tempor incididunt ut aliquip ipsum ipsum qui nulla incididunt. Labore eiusmod excepteur qui esse do ipsum cupidatat. Minim esse in culpa ex.
+        </p>
+      </div>
+
+      <div>
+        <h2>Testimonials</h2>
+        
+      </div>
+
+    </>
+  )
+}
+
+export default App
