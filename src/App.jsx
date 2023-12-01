13ba4d3e647ed23c11939aad692c2e27d802388e
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-12-01 16:49:51 -0500

    rough layout complete

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,16 @@ function App() {
       <div className="parallax">
 
         <header>
-          <h1>Spark*Coaching</h1>
+          <div className="title">
+            <h1>Spark*</h1>
+            <h1>Coaching</h1>
+          </div>
         </header>
 
         <div className="parallax__stevie1">
         </div>
 
-        <div className="parallax__about">
+        <div id="info" className="parallax__about">
           <h2>About</h2>
           <div className="text-box">
             <p>Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia. Nulla ex sunt eiusmod quis. Tempor laborum qui tempor incididunt ut aliquip ipsum ipsum qui nulla incididunt. Labore eiusmod excepteur qui esse do ipsum cupidatat. Minim esse in culpa ex.
@@ -27,7 +30,7 @@ function App() {
         </div>
 
 
-        <div className="parallax__services">
+        <div id="info" className="parallax__services">
           <h2>Services</h2>
           <div className="text-box">
             <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
@@ -36,7 +39,7 @@ function App() {
 
         <div className="parallax__stevie3"></div>
 
-        <div className="parallax__testimonial1">
+        <div id="info" className="parallax__testimonial1">
           <h2>Testimonial A</h2>
           <div className="text-box">
             <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
@@ -45,7 +48,7 @@ function App() {
 
         <div className="parallax__stevie4"></div>
 
-        <div className="parallax__testimonial2">
+        <div id="info" className="parallax__testimonial2">
           <h2>Testimonial B</h2>
           <div className="text-box">
             <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
@@ -54,7 +57,7 @@ function App() {
 
         <div className="parallax__stevie5"></div>
 
-        <div className="parallax__testimonial3">
+        <div id="info" className="parallax__testimonial3">
           <h2>Testimonial C</h2>
           <div className="text-box">
             <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
@@ -65,7 +68,7 @@ function App() {
 
       </div>
 
-      <footer>
+      <footer id="info">
         <h2>Footer</h2>
         <div className="text-box">
           <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
