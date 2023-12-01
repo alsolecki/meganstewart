123a494cf2cc6849858f8ca919c001b3982a8824
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-11-30 23:27:14 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,79 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
 
+
   return (
     <>
-      <div>
-        <h1>Megan Stewart</h1>
-        <p>Executive Coaching Professional</p>
+      <div className="parallax">
+
+        <header>
+          <h1>Spark*Coaching</h1>
+        </header>
+
+        <div className="parallax__stevie1">
+        </div>
+
+        <div className="parallax__about">
+          <h2>About</h2>
+          <div className="text-box">
+            <p>Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia. Nulla ex sunt eiusmod quis. Tempor laborum qui tempor incididunt ut aliquip ipsum ipsum qui nulla incididunt. Labore eiusmod excepteur qui esse do ipsum cupidatat. Minim esse in culpa ex.
+            </p>
+          </div>
+        </div>
+
+        <div className="parallax__stevie2">
+        </div>
+
+
+        <div className="parallax__services">
+          <h2>Services</h2>
+          <div className="text-box">
+            <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+          </div>
+        </div>
+
+        <div className="parallax__stevie3"></div>
+
+        <div className="parallax__testimonial1">
+          <h2>Testimonial A</h2>
+          <div className="text-box">
+            <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+          </div>
+        </div>
+
+        <div className="parallax__stevie4"></div>
+
+        <div className="parallax__testimonial2">
+          <h2>Testimonial B</h2>
+          <div className="text-box">
+            <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+          </div>
+        </div>
+
+        <div className="parallax__stevie5"></div>
+
+        <div className="parallax__testimonial3">
+          <h2>Testimonial C</h2>
+          <div className="text-box">
+            <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+          </div>
+        </div>
+
+        <div className="parallax__stevie6"></div>
+
       </div>
 
-      <div>
-        <h2>About</h2>
-        <p>Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia. Nulla ex sunt eiusmod quis. Tempor laborum qui tempor incididunt ut aliquip ipsum ipsum qui nulla incididunt. Labore eiusmod excepteur qui esse do ipsum cupidatat. Minim esse in culpa ex.
-        </p>
-      </div>
-
-      <div>
-        <h2>Testimonials</h2>
-        
-      </div>
-
+      <footer>
+        <h2>Footer</h2>
+        <div className="text-box">
+          <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+        </div>
+        <div className="text-box">
+          <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+        </div>
+      </footer>
     </>
   )
 }
