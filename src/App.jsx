6f4758a1d8f1761a07941c8f04003135a8b9927c
Alex Solecki <129791664+alsolecki@users.noreply.github.com>
2023-12-01 19:48:09 -0500

    changed to perspective parallax

--- src/App.jsx
+++ src/App.jsx
@@ -6,77 +6,77 @@ function App() {
 
   return (
     <>
-      <div className="parallax">
+      <div className="wrapper">
+        <div className="parallax">
 
-        <header>
-          <div className="title">
-            <h1>Spark*</h1>
-            <h1>Coaching</h1>
+          <header>
+            <div className="title">
+              <h1>Spark*</h1>
+              <h1>Coaching</h1>
+            </div>
+          </header>
+
+          <div className="parallax__stevie1">
           </div>
-        </header>
 
-        <div className="parallax__stevie1">
-        </div>
-
-        <div id="info" className="parallax__about">
-          <h2>About</h2>
-          <div className="text-box">
-            <p>Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia. Nulla ex sunt eiusmod quis. Tempor laborum qui tempor incididunt ut aliquip ipsum ipsum qui nulla incididunt. Labore eiusmod excepteur qui esse do ipsum cupidatat. Minim esse in culpa ex.
-            </p>
+          <div id="info" className="parallax__about">
+            <div className="text-box">
+            <h2>About</h2>
+              <p>Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia. Nulla ex sunt eiusmod quis. Tempor laborum qui tempor incididunt ut aliquip ipsum ipsum qui nulla incididunt. Labore eiusmod excepteur qui esse do ipsum cupidatat. Minim esse in culpa ex.
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className="parallax__stevie2">
-        </div>
-
-
-        <div id="info" className="parallax__services">
-          <h2>Services</h2>
-          <div className="text-box">
-            <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+          <div className="parallax__stevie2">
           </div>
-        </div>
 
-        <div className="parallax__stevie3"></div>
-
-        <div id="info" className="parallax__testimonial1">
-          <h2>Testimonial A</h2>
-          <div className="text-box">
-            <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+          <div id="info" className="parallax__services">
+            <div className="text-box">
+            <h2>Services</h2>
+              <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+            </div>
           </div>
-        </div>
 
-        <div className="parallax__stevie4"></div>
+          <div className="parallax__stevie3"></div>
 
-        <div id="info" className="parallax__testimonial2">
-          <h2>Testimonial B</h2>
-          <div className="text-box">
-            <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+          <div id="info" className="parallax__testimonial1">
+            <div className="text-box">
+            <h2>Testimonial A</h2>
+              <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+            </div>
           </div>
-        </div>
 
-        <div className="parallax__stevie5"></div>
+          <div className="parallax__stevie4"></div>
 
-        <div id="info" className="parallax__testimonial3">
-          <h2>Testimonial C</h2>
-          <div className="text-box">
-            <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+          <div id="info" className="parallax__testimonial2">
+            <div className="text-box">
+            <h2>Testimonial B</h2>
+              <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+            </div>
           </div>
+
+          <div className="parallax__stevie5"></div>
+
+          <div id="info" className="parallax__testimonial3">
+            <div className="text-box">
+            <h2>Testimonial C</h2>
+              <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+            </div>
+          </div>
+
+          <div className="parallax__stevie6"></div>
+
+          <footer id="info">
+            <div className="text-box">
+            <h2>Footer</h2>
+              <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+            </div>
+            <div className="text-box">
+              <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
+            </div>
+          </footer>
         </div>
-
-        <div className="parallax__stevie6"></div>
-
       </div>
-
-      <footer id="info">
-        <h2>Footer</h2>
-        <div className="text-box">
-          <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
-        </div>
-        <div className="text-box">
-          <p>"Non cillum adipisicing occaecat et pariatur esse sint deserunt cupidatat dolor amet ad velit nostrud. Irure laborum est dolore officia aliqua reprehenderit veniam ipsum magna cupidatat consectetur commodo eu. Proident irure voluptate magna dolor nulla reprehenderit ullamco officia."</p>
-        </div>
-      </footer>
     </>
   )
 }
